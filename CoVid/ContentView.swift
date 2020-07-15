92694ee40f22fff718e6b58566a64ea5d3082c53
Linda <linda.zungu@icloud.com>
2020-07-15 11:28:52 +0200

    Initial Commit

---
+++ CoVid/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoVid
+//
+//  Created by Linda Zungu on 7/15/20.
+//  Copyright © 2020 Linda Zungu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
