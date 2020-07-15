7404178229c2d451edf1a32c7ae7ea946e44e709
Linda <linda.zungu@icloud.com>
2020-07-15 13:36:12 +0200

    Initial Commit from me

---
+++ CoVid/MyImageView.swift
@@ -0,0 +1,21 @@
+//
+//  MyImageView.swift
+//  CoVid
+//
+//  Created by Linda Zungu on 7/15/20.
+//  Copyright © 2020 Linda Zungu. All rights reserved.
+//
+
+import SwiftUI
+
+struct MyImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MyImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyImageView()
+    }
+}
---
+++ CoVid/SummaryView.swift
@@ -0,0 +1,21 @@
+//
+//  SummaryView.swift
+//  CoVid
+//
+//  Created by Linda Zungu on 7/15/20.
+//  Copyright © 2020 Linda Zungu. All rights reserved.
+//
+
+import SwiftUI
+
+struct SummaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SummaryView_Previews: PreviewProvider {
+    static var previews: some View {
+        SummaryView()
+    }
+}
