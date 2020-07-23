9ed50b276b3348f8be213beec09a429da65af159
Linda <linda.zungu@icloud.com>
2020-07-23 03:33:32 +0200

    New CountrySummaryView added, shows the specific country stats

---
+++ CoVid/CountrySummaryView.swift
@@ -0,0 +1,21 @@
+//
+//  CountrySummaryView.swift
+//  CoVid
+//
+//  Created by Linda Zungu on 7/22/20.
+//  Copyright © 2020 Linda Zungu. All rights reserved.
+//
+
+import SwiftUI
+
+struct CountrySummaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CountrySummaryView_Previews: PreviewProvider {
+    static var previews: some View {
+        CountrySummaryView()
+    }
+}
