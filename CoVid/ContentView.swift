a99385d3ffc51b22b19785c863b61ca3caea36dd
Linda <linda.zungu@icloud.com>
2020-07-31 01:48:36 +0200

    Created a tabview to switch between multiple views.

--- CoVid/ContentView.swift
+++ CoVid/ContentView.swift
@@ -39,82 +39,86 @@ struct ContentView: View {
     @State var countRefreshes = 1
     
     var body: some View {
-        ScrollView(.vertical, showsIndicators: false){
-            VStack(alignment: .leading){
-                ZStack(alignment: .center){
-                    VStack{
-                        if removeLaunchScreen == false {
-                            BlurView(style: .systemUltraThinMaterial)
-                            .frame(height: checkHeight() ? 330 : 280)
-                            .cornerRadius(checkHeight() ? 30 : 0)
-                            .shadow(radius: 30)
-                            .overlay(
-                                LottieView(fileName: "20546-i-stay-at-home", loopMode: .repeat(2))
-                            )
-                        }
-                        
-                        if summary.count > 0 {
-                            CountrySummaryView(countryTotalCases: summary[selectedCountry].TotalConfirmed, countryDeathCases: summary[selectedCountry].TotalDeaths, countryRecoveredCases: summary[selectedCountry].TotalRecovered, countryNewTotalCases: summary[selectedCountry].NewConfirmed, countryNewDeathCases: summary[selectedCountry].NewDeaths, countryNewRecoveriesCases: summary[selectedCountry].NewRecovered)
-                                .padding(.top, 20)
-                        }
-                        else{
-                            CountrySummaryView(countryTotalCases: 0, countryDeathCases: 0, countryRecoveredCases: 0, countryNewTotalCases: 0, countryNewDeathCases: 0, countryNewRecoveriesCases: 0)
-                            .padding(.top, 20)
-                        }
-                        
-                        SummaryView(globalTotalCases: globalTCases/(self.countRefreshes), globalDeathCases: globalDCases/(self.countRefreshes), globalRecoveredCases: globalRCases/(self.countRefreshes))
-                    }
-                    Button(action: {
-                        if self.summary.count > 0 {
-                            self.show.toggle()
-                        }
-                        else{
-                            self.countRefreshes += 1
-                            self.loadData()
-                        }
-                    }, label: {
-                        ZStack{
-                            Text((summary.count > 0) ? summary[selectedCountry].Country : "Refresh")
-                                .padding(.bottom, show ? 0 : 250)
-                                .frame(minWidth: show ? .leastNonzeroMagnitude+130 : UIScreen.main.bounds.width-190, maxWidth: show ? UIScreen.main.bounds.width : UIScreen.main.bounds.width-190, minHeight: 0, maxHeight: 270)
-                                .font(.headline)
-                                .foregroundColor((colorScheme == .dark) ? .white : .black)
-                                .padding()
-                                .padding(.horizontal, 30)
-                                .fixedSize()
-                                .background(
+        ZStack{
+            TabView{
+                ScrollView(.vertical, showsIndicators: false){
+                    VStack(alignment: .leading){
+                        ZStack(alignment: .center){
+                            VStack{
+                                if removeLaunchScreen == false {
                                     BlurView(style: .systemUltraThinMaterial)
-                                        .cornerRadius(40)
-                                        .shadow(radius: 30, y: 7)
-                                        .padding(.horizontal, show ? 20 : -22)
-                            )
-                                .multilineTextAlignment(.center).lineLimit(1).minimumScaleFactor(0.6)
-                            
-                            if show == false{
-                                Picker(selection: $selectedCountry, label: Text("Choose Country"), content: {
-                                    ForEach(0..<summary.count) {
-                                        Text(self.summary[$0].Country)
-                                    }
-                                })
-                                    .labelsHidden()
-                                    .opacity(show ? 0 : 1)
+                                    .frame(height: checkHeight() ? 330 : 280)
+                                    .cornerRadius(checkHeight() ? 30 : 0)
+                                    .shadow(radius: 30)
+                                    .overlay(
+                                        LottieView(fileName: "20546-i-stay-at-home", loopMode: .repeat(2))
+                                    )
+                                }
+                                
+                                if summary.count > 0 {
+                                    CountrySummaryView(countryTotalCases: summary[selectedCountry].TotalConfirmed, countryDeathCases: summary[selectedCountry].TotalDeaths, countryRecoveredCases: summary[selectedCountry].TotalRecovered, countryNewTotalCases: summary[selectedCountry].NewConfirmed, countryNewDeathCases: summary[selectedCountry].NewDeaths, countryNewRecoveriesCases: summary[selectedCountry].NewRecovered)
+                                        .padding(.top, 20)
+                                }
+                                else{
+                                    CountrySummaryView(countryTotalCases: 0, countryDeathCases: 0, countryRecoveredCases: 0, countryNewTotalCases: 0, countryNewDeathCases: 0, countryNewRecoveriesCases: 0)
+                                    .padding(.top, 20)
+                                }
+                                
+                                SummaryView(globalTotalCases: globalTCases/(self.countRefreshes), globalDeathCases: globalDCases/(self.countRefreshes), globalRecoveredCases: globalRCases/(self.countRefreshes))
                             }
+                            Button(action: {
+                                if self.summary.count > 0 {
+                                    self.show.toggle()
+                                }
+                                else{
+                                    self.countRefreshes += 1
+                                    self.loadData()
+                                }
+                            }, label: {
+                                ZStack{
+                                    Text((summary.count > 0) ? summary[selectedCountry].Country : "Refresh")
+                                        .padding(.bottom, show ? 0 : 250)
+                                        .frame(minWidth: show ? .leastNonzeroMagnitude+130 : UIScreen.main.bounds.width-190, maxWidth: show ? UIScreen.main.bounds.width : UIScreen.main.bounds.width-190, minHeight: 0, maxHeight: 270)
+                                        .font(.headline)
+                                        .foregroundColor((colorScheme == .dark) ? .white : .black)
+                                        .padding()
+                                        .padding(.horizontal, 30)
+                                        .fixedSize()
+                                        .background(
+                                            BlurView(style: .systemUltraThinMaterial)
+                                                .cornerRadius(40)
+                                                .shadow(radius: 30, y: 7)
+                                                .padding(.horizontal, show ? 20 : -22)
+                                    )
+                                        .multilineTextAlignment(.center).lineLimit(1).minimumScaleFactor(0.6)
+                                    
+                                    if show == false{
+                                        Picker(selection: $selectedCountry, label: Text("Choose Country"), content: {
+                                            ForEach(0..<summary.count) {
+                                                Text(self.summary[$0].Country)
+                                            }
+                                        })
+                                            .labelsHidden()
+                                            .opacity(show ? 0 : 1)
+                                    }
+                                }
+                            })
+                                .animation(.spring(response: 0.4, dampingFraction: 0.75, blendDuration: 0.9))
+                                .padding(.bottom, checkHeight() ? 370 : 420)
                         }
-                    })
-                        .animation(.spring(response: 0.4, dampingFraction: 0.75, blendDuration: 0.9))
-                        .padding(.bottom, checkHeight() ? 370 : 420)
-                    
-                    StartupView()
-                        .frame(height: removeLaunchScreen ? UIScreen.main.bounds.height : 0)
-                        .opacity(removeLaunchScreen ? 1 : 0)
-                        .animation(Animation.easeInOut(duration: 0.8))
-                        .onAppear(perform: remove)
+                        Spacer(minLength: 30)
+                    }
+                    .frame(width: UIScreen.main.bounds.width)
                 }
-                Spacer(minLength: 30)
+                .edgesIgnoringSafeArea(.top)
             }
-            .frame(width: UIScreen.main.bounds.width)
+            
+            StartupView()
+            .frame(height: removeLaunchScreen ? UIScreen.main.bounds.height : 0)
+            .opacity(removeLaunchScreen ? 1 : 0)
+            .animation(Animation.easeInOut(duration: 0.8))
+            .onAppear(perform: remove)
         }
-        .edgesIgnoringSafeArea(.top)
         .onAppear(perform: loadData)
     }
     
@@ -161,6 +165,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()//.colorScheme(.dark)
+        ContentView().colorScheme(.dark)
     }
 }
