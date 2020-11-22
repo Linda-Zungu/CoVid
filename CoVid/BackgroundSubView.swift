d41a3b0843ff3603b901232eff940c2ec73f2aff
Linda <linda.zungu@icloud.com>
2020-11-23 00:02:29 +0200

    Background for all the summary views and random tip view

---
+++ CoVid/BackgroundSubView.swift
@@ -0,0 +1,21 @@
+//
+//  BackgroundSubView.swift
+//  CoVid
+//
+//  Created by Linda Zungu on 11/22/20.
+//  Copyright © 2020 Linda Zungu. All rights reserved.
+//
+
+import SwiftUI
+
+struct BackgroundSubView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BackgroundSubView_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundSubView()
+    }
+}
