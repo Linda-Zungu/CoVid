ee0d15a1c4f03afc878b05a0b374995e12123008
Linda <linda.zungu@icloud.com>
2020-12-05 05:39:21 +0200

    Reload when opening the app again if the date is next day

--- CoVid/SceneDelegate.swift
+++ CoVid/SceneDelegate.swift
@@ -12,6 +12,7 @@ import SwiftUI
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    let now = Date()
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
@@ -51,6 +52,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
+        
+        print(Calendar.current.isDateInTomorrow(now))
+        if(Calendar.current.isDateInTomorrow(now)){
+            let contentView = ContentView()
+            if let windowScene = scene as? UIWindowScene {
+                let window = UIWindow(windowScene: windowScene)
+                window.rootViewController = UIHostingController(rootView: contentView)
+                self.window = window
+                window.makeKeyAndVisible()
+            }
+        }
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
